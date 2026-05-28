6efffa9d8f43c851cd6f6b66138ce3029f48c6fa
Abuzar <abuzar@Abuzars-MacBook-Air.local>
2026-05-29 00:48:01 +0500

    done

--- public-website/src/config/app.config.ts
+++ public-website/src/config/app.config.ts
@@ -1,6 +1,6 @@
 export const APP_CONFIG = {
   appName: "Loan Tracker",
-  apkDownloadUrl: "https://github.com/abuzarkhan1/Loan-Tracker/releases/download/V1/application-8fac4b83-a019-46d3-b744-61d2a3dfee2b.apk",
+  apkDownloadUrl: "https://github.com/abuzarkhan1/Loan-Tracker/releases/download/V1/application-9fb0d8b5-2a99-4bc8-bbe1-28ec1f2f36c9.apk",
   apkVersion: "V1",
   apkSize: "97.8 MB",
   lastUpdated: "May 28, 2026",
